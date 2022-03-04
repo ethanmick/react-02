8962c75387adddcca4ece2f3e2ee1165cd85db05
Ethan Mick <ethan@ethanmick.com>
2022-03-04 09:54:18 -0800

    Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,2 +1,21 @@
-const App = () => <>Hello World</>
+import './App.css'
+
+interface Todo {
+  text: string
+}
+
+const todos: Todo[] = [
+  { text: 'Learn React' },
+  { text: 'Learn TypeScript' },
+  { text: 'Go outside' }
+]
+
+const App = () => (
+  <ul>
+    {todos.map((todo) => (
+      <li>{todo.text}</li>
+    ))}
+  </ul>
+)
+
 export default App
